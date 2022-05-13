3eed8be33d767332602d80c7eaeaed02a51b7c7a
Tzu Yin <tzyn.wang@gmail.com>
2022-05-13 22:43:19 +0800

    [feat] Update Body content

--- src/components/Common/Body/index.tsx
+++ src/components/Common/Body/index.tsx
@@ -1,15 +1,23 @@
 import React, { memo } from 'react';
 import { styled } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { red, purple, pink } from '@mui/material/colors';
+import BodyBlock from '@Components/Common/BodyBlock';
 
-const StyledBody = styled('div')(() => ({
-  height: '200vh',
+const BodyContainer = styled('div')(() => ({
   paddingTop: '54px',
-  backgroundColor: red[100],
+  backgroundColor: purple[50],
 }));
 
 function Body(): React.ReactElement {
-  return <StyledBody>body</StyledBody>;
+  // Main
+  return (
+    <BodyContainer>
+      <BodyBlock bgColor={purple[50]}>block 1</BodyBlock>
+      <BodyBlock bgColor={red[50]}>block 2</BodyBlock>
+      <BodyBlock bgColor={pink[200]}>block 3</BodyBlock>
+      <BodyBlock>block 4</BodyBlock>
+    </BodyContainer>
+  );
 }
 
 export default memo(Body);
