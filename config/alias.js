d66f8f074b2cb087bfc43a3ae4adea87b46c4ef7
Tzu Yin <tzyn.wang@gmail.com>
2022-05-13 21:49:41 +0800

    [feat] alias add @Hooks

--- config/alias.js
+++ config/alias.js
@@ -2,5 +2,6 @@ const path = require('path');
 
 module.exports = {
   '@Components': path.resolve(__dirname, '..', './src/components'),
+  '@Hooks': path.resolve(__dirname, '..', './src/hooks'),
   '@Theme': path.resolve(__dirname, '..', './src/theme'),
 };
