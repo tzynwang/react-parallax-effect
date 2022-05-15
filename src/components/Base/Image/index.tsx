5c0b7494fef8bc3eb2ecd6869d431f693a62bf13
Tzu Yin <tzyn.wang@gmail.com>
2022-05-15 14:25:32 +0800

    [feat] Update opacity by inView

--- src/components/Base/Image/index.tsx
+++ src/components/Base/Image/index.tsx
@@ -1,7 +1,22 @@
 import { styled } from '@mui/material/styles';
 
-export default styled('img')(() => ({
+interface ImgProps {
+  inView?: boolean;
+}
+
+export default styled('img', {
+  shouldForwardProp: (prop) =>
+    prop !== 'inView' && prop !== 'scrollTop' && prop !== 'viewportHeight',
+})<ImgProps>(({ inView, theme }) => ({
   width: '100%',
   display: 'block',
   objectFit: 'cover',
+  opacity: inView ? 1 : 0,
+  // transform:
+  //   inView && scrollTop && viewportHeight
+  //     ? `translateY(${(scrollTop - viewportHeight) / 2}px)`
+  //     : 'unset',
+  transitionProperty: 'opacity',
+  transitionDuration: `${theme.transitions.duration.enteringScreen}ms`,
+  transitionTimingFunction: theme.transitions.easing.sharp,
 }));
